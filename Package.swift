0460aa14c36fa7600b0e496ff96a00b4cef37a34
Aaditya Chandrasekhar <aadityac@gmail.com>
2022-09-27 06:17:40 +0000

    Import cmark-gfm from swift-cmark package.

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CommonMark",
-            dependencies: ["cmark-gfm"]
+            dependencies: [.product(name: "cmark-gfm", package: "swift-cmark")]
         ),
         .target(
             name: "CommonMarkBuilder",
